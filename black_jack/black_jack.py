390b45c10ea6135061fc8e6f3e009df5841f4aa5
Daniel Chin <DanChin327@gmail.com>
2024-08-28 15:42:21 +0900

    working on classes

--- black_jack/black_jack.py
+++ black_jack/black_jack.py
@@ -14,149 +14,109 @@ DEALER_STAND = 17
 INITIAL_FUNDS = 20
 BLACKJACK_PAYOUT = 1.5
 
-# Card class representing a single card
-class Card:
-    def __init__(self, suit, rank, value):
-        self.suit = suit
-        self.rank = rank
-        self.value = value
-
-    def __repr__(self):
-        return f"{self.rank} of {self.suit}"
-
-# Deck class representing a deck of cards
-class Deck:
+# Function to create a deck of cards
+def create_deck():
     suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
     ranks = {
         '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
         '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11
     }
+    deck = [{'suit': suit, 'rank': rank, 'value': value} for suit in suits for rank, value in ranks.items()]
+    random.shuffle(deck)
+    return deck
 
-    def __init__(self):
-        self.cards = [Card(suit, rank, value) for suit in self.suits for rank, value in self.ranks.items()]
-        self.shuffle()
+# Function to calculate the value of a hand
+def calculate_hand_value(hand):
+    value = sum(card['value'] for card in hand)
+    aces = sum(1 for card in hand if card['rank'] == 'A')
+    while value > BLACKJACK and aces:
+        value -= 10
+        aces -= 1
+    return value
 
-    def shuffle(self):
-        random.shuffle(self.cards)
+# Function to deal a card from the deck
+def deal_card(deck):
+    return deck.pop()
 
-    def deal(self):
-        return self.cards.pop() if self.cards else None
+# Function to play a round of Blackjack
+def play_round(player_funds, deck):
+    # Check if player has funds
+    if player_funds <= 0:
+        print("Game Over! You have no funds left.")
+        return player_funds, False
 
-# Player class representing a player
-class Player:
-    def __init__(self):
-        self.hand = []
-        self.funds = INITIAL_FUNDS
-        self.current_bet = 0
+    # Get player's bet
+    bet = int(input(f"You have {player_funds} funds. Enter your bet: "))
+    if bet > player_funds:
+        print("Insufficient funds for that bet. Try again.")
+        return player_funds, True
 
-    def place_bet(self, amount):
-        if amount <= self.funds:
-            self.current_bet = amount
-            self.funds -= amount
+    # Initial hands
+    player_hand = [deal_card(deck), deal_card(deck)]
+    dealer_hand = [deal_card(deck), deal_card(deck)]
+
+    # Show initial hands
+    print(f"Your hand: {player_hand} - Value: {calculate_hand_value(player_hand)}")
+    print(f"Dealer's hand: [{dealer_hand[0]}, ?]")
+
+    # Player's turn
+    while calculate_hand_value(player_hand) < BLACKJACK:
+        action = input("Do you want to hit or stand? (h/s): ").lower()
+        if action == 'h':
+            player_hand.append(deal_card(deck))
+            print(f"Your hand: {player_hand} - Value: {calculate_hand_value(player_hand)}")
+            if calculate_hand_value(player_hand) > BLACKJACK:
+                print("You busted!")
+                player_funds -= bet
+                return player_funds, True
         else:
-            print("Insufficient funds.")
+            break
 
-    def add_card(self, card):
-        self.hand.append(card)
+    # Dealer's turn
+    while calculate_hand_value(dealer_hand) < DEALER_STAND:
+        dealer_hand.append(deal_card(deck))
 
-    def hand_value(self):
-        value = sum(card.value for card in self.hand)
-        # Adjust for Aces
-        aces = sum(card.rank == 'A' for card in self.hand)
-        while value > BLACKJACK and aces:
-            value -= 10
-            aces -= 1
-        return value
+    print(f"Dealer's hand: {dealer_hand} - Value: {calculate_hand_value(dealer_hand)}")
 
-    def clear_hand(self):
-        self.hand = []
+    # Determine winner
+    player_value = calculate_hand_value(player_hand)
+    dealer_value = calculate_hand_value(dealer_hand)
 
-# Dealer class, similar to player but with different behavior
-class Dealer(Player):
-    def should_hit(self):
-        return self.hand_value() < DEALER_STAND
+    if player_value > BLACKJACK:
+        print("You lose!")
+        player_funds -= bet
+    elif dealer_value > BLACKJACK or player_value > dealer_value:
+        winnings = bet * (BLACKJACK_PAYOUT if player_value == BLACKJACK else 1)
+        player_funds += winnings
+        print(f"You win! Your winnings: {winnings}")
+    elif player_value == dealer_value:
+        print("It's a tie! Your bet is returned.")
+    else:
+        print("You lose!")
+        player_funds -= bet
 
-# Game class to manage the flow of the game
-class BlackjackGame:
-    def __init__(self):
-        self.deck = Deck()
-        self.player = Player()
-        self.dealer = Dealer()
+    # Reshuffle if the deck is low on cards
+    if len(deck) < 10:
+        deck = create_deck()
+        print("Deck reshuffled!")
 
-    def play_round(self):
-        # Check funds
-        if self.player.funds <= 0:
-            print("Game Over! You have no funds left.")
-            return False
+    # Check winning condition
+    if player_funds >= 100:
+        print("Congratulations! You reached 100 funds. You win the game!")
+        return player_funds, False
 
-        # Get player's bet
-        bet = int(input(f"You have {self.player.funds} funds. Enter your bet: "))
-        self.player.place_bet(bet)
+    return player_funds, True
 
-        # Deal initial cards
-        self.player.add_card(self.deck.deal())
-        self.dealer.add_card(self.deck.deal())
-        self.player.add_card(self.deck.deal())
-        self.dealer.add_card(self.deck.deal())
+# Main function to run the game
+def main():
+    player_funds = INITIAL_FUNDS
+    deck = create_deck()
+    playing = True
 
-        # Show initial hands
-        print(f"Player's Hand: {self.player.hand} - Value: {self.player.hand_value()}")
-        print(f"Dealer's Hand: [{self.dealer.hand[0]}, ?]")
+    while playing:
+        player_funds, playing = play_round(player_funds, deck)
 
-        # Player's turn
-        while self.player.hand_value() < BLACKJACK:
-            action = input("Do you want to hit or stand? (h/s): ").lower()
-            if action == 'h':
-                self.player.add_card(self.deck.deal())
-                print(f"Player's Hand: {self.player.hand} - Value: {self.player.hand_value()}")
-                if self.player.hand_value() > BLACKJACK:
-                    print("You busted!")
-                    break
-            else:
-                break
-
-        # Dealer's turn
-        if self.player.hand_value() <= BLACKJACK:
-            while self.dealer.should_hit():
-                self.dealer.add_card(self.deck.deal())
-
-            print(f"Dealer's Hand: {self.dealer.hand} - Value: {self.dealer.hand_value()}")
-
-        # Determine winner
-        player_value = self.player.hand_value()
-        dealer_value = self.dealer.hand_value()
-
-        if player_value > BLACKJACK:
-            print("You lose!")
-        elif dealer_value > BLACKJACK or player_value > dealer_value:
-            winnings = self.player.current_bet * (BLACKJACK_PAYOUT if player_value == BLACKJACK else 1)
-            self.player.funds += winnings
-            print(f"You win! Your winnings: {winnings}")
-        elif player_value == dealer_value:
-            self.player.funds += self.player.current_bet  # Return bet
-            print("It's a tie!")
-        else:
-            print("You lose!")
-
-        # Clear hands for next round
-        self.player.clear_hand()
-        self.dealer.clear_hand()
-
-        # Reshuffle if the deck is low on cards
-        if len(self.deck.cards) < 10:
-            self.deck = Deck()
-            print("Deck reshuffled!")
-
-        # Check winning condition
-        if self.player.funds >= 100:
-            print("Congratulations! You reached 100 funds. You win the game!")
-            return False
-
-        return True
+    print("Thank you for playing Blackjack!")
 
 # Run the game
-game = BlackjackGame()
-while game.play_round():
-    continue
-
-print("Thank you for playing Blackjack!")
+main()
