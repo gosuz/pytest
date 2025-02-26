1100f627c3e57a79060a42dcbdf81d993a29abf4
gosuz <go.suz55@gmail.com>
2025-02-26 19:11:10 +0800

    change code so it takes user input to create a list of times which are then added together

--- time_calculator_sorted/time_adder.py
+++ time_calculator_sorted/time_adder.py
@@ -6,33 +6,6 @@
     # [0, 50], [1, 20], [3, 5]       [hrs, min]
 
 
-def get_user_input():
-    # create a list
-    times_list = []
-    user_input_1 = 0
-    while (user_input_1 != 'Y'):
-            user_input_1 = input("Enter the hour(s)   or enter 'Y' if you're done: ")
-            if (user_input_1 or user_input_2) == 'Y':
-                return f"Displays time  list: {times_list} "
-
-            user_input_2 = input("Enter the minute(s):      ")
-
-            int_user_input_1 = int(user_input_1)
-            int_user_input_2 = int(user_input_2)
-
-            try:
-                if user_input_2 >= 60:
-                    print("Please enter the value again")
-            except:
-                print("input: ")
-
-            time = [int_user_input_1, ]
-            print(time)
-            times_list += [time]
-            print(times_list)
-
-get_user_input()
-
 # shouldn't accept input2(minutes) to be greater than 59
 
 
@@ -41,29 +14,38 @@ get_user_input()
 
 # another possible scenario is the user gives a blank input. Should handle the value as 0 or ask them to reenter value
 
-def time_list_creator(hr, min):
-    """Create a list of hr and min
 
-    :param hr: hour
-    :param min: minute
+
+def time_list_creator():
+    """Ask user for input (for hours and minutes) until they type 'q'. Then create a list of hr and min
+
+    :param N/A
 
     :return: list - hr and minute integer value.
     """
     # If minute paramater is greater than 59, create an empty list
 
     # change this so it's interactive. Gets input from prompt
+    time_list = []
 
-    # int_hr = int(hr)
-    # int_min = int(min)
-    # # print(int_hr)
-    # # print(int_min)
+    while True:
+        hr = input("Enter hour(s) or type 'q' to quit: ")
+        if hr == "q":
+            print(time_list)
+            return time_list #the list up to this point e.g [[1,43], [2,26], [3,10]]
 
-    if int_min <= 59:
-        # create the list
-        hr_min = [int_hr, int_min]
-        # print([int_hr, int_min])
-        return [int_hr, int_min]
+        minutes = input("Enter minnute(s): ")
 
+        try:
+            hr_input = int(hr)
+            min_input = int(minutes)
+            time = [hr_input, min_input]
+            time_list.append(time)
+        except:
+            print("Invalid input. Please enter whole numbers")
+
+        print(time_list)
+    return time_list
 
 def check_min_greater_than_59(minutes):
     """ Checks if the minutes is greater than 59. Converts it into hrs and minutes if it is.
@@ -73,6 +55,7 @@ def check_min_greater_than_59(minutes):
 
     If it is, it converts it into __hrs __ min
     """
+
     # if it isn't greater than 59, return [0, min] #=> [0, 59]
 
     # 1. convert minutes into integer
@@ -141,13 +124,17 @@ def time_adder(list_of_times):
         # print(total_mins[1], "result of: total_mins[1]") # gets minutes
 
     total_list = [total_hrs, total_mins[1]]
+    # print(f"total_hrs and total_mins{total_hrs}{total_mins}")
+    # print(f"total_list: {total_list}")
     print(display_as_string(total_list))
     return display_as_string(total_list)
 
     print("checking time_adder")
 
+list_of_times = time_list_creator()
+time_adder(list_of_times)
 
-time_adder([[0,55],[0,33],[1,50]])
+# time_adder([[0,55],[0,33],[1,50]])
 
 
 # format we need to store data time_adder([[0,55],[0,33],[0,50]])
