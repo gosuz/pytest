c496132afef61c6e1154708fdf000d86c11a36e1
gosuz <go.suz55@gmail.com>
2025-03-04 20:58:29 +0800

    added git ignore, and wrote unit test for time_list_creator

--- time_calculator_sorted/time_calculator_tester.py
+++ time_calculator_sorted/time_calculator_tester.py
@@ -19,3 +19,29 @@ TLDR:
         2. Input + substraction
         3. Input + addition + substraction
 '''
+
+
+'''
+Unit Tests for time_adder.py
+- Unit tests will check that all my functions work as intended
+
+Unit tests include:
+    1. time_list_creator - what do I need to test exactly?
+        - The output should always return a list in the format of [[1,10],[3, 35], [5,17]]
+        output: always a list
+        - items in each list within the list should ONLY be integers
+
+    2. check_min_greater_than_59(minutes)
+        - should always return a list in one of the following formats:
+            2.1 [0, 59]
+            2.2 [0, 70] => [1, 10]
+        - the minutes value when returned should never exceed 59. (0-59 are valid values when returned)
+
+    3. display_as_string(time)
+        - should always display as a string:
+            Total time: __ hrs __ minute(s).
+
+    4. time_adder(list_of_times)
+        - should get the numbers and the output should return the total hrs and minutes. e.g 4hrs 30min
+    !! We still need to check the logic for time_adder is correct
+'''
